a7ab7722c1734bf8e76911a1483869794a6e9260
승민 황 <iqmin11@gmail.com>
2024-04-13 16:34:20 +0900

    황승민 : 레플리케이션 숫강

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority())
+	{
+		FVector CurLocation = GetActorLocation();
+		CurLocation += FVector(Velocity * DeltaTime, 0, 0);
+		SetActorLocation(CurLocation);
+	}
+}
---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	AMovingPlatform();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	//UPROPERTY()
+	//FVector Dir;
+
+	UPROPERTY(EditAnywhere, Category = "Move")
+	float Velocity = 5.f;
+};
